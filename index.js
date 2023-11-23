af4c85f3e96ddff68f3a16d05f70c464a9b50522
krishundre <krish.undre@somaiya.edu>
2023-11-23 22:09:23 +0530

    contact & about sec done now working on services

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
-function SendMail(){
-    var paras= {
-      from_name : document.getElementById("fullName").value,
-      email_id : document.getElementByID("email_id").value,
-      message: document.getElementById("message").value
-    }
-  emailjs.send("service_h9fc05o","template_kqdqxfq",paras).then(function(res){
-    alert("Success!!" + res.status);
-  })
+function SendMail() {
+  var paras = {
+    from_name: document.getElementById("fullName").value,
+    email_id: document.getElementByID("email_id").value,
+    message: document.getElementById("message").value,
+  };
+  emailjs
+    .send("service_h9fc05o", "template_kqdqxfq", paras)
+    .then(function (res) {
+      alert("Success!!" + res.status);
+    });
+}
+
+function submitForm() {
+  const submitButton = document.getElementById("submitBtn");
+  const messageInput = document.getElementById("message");
+  const submittedMessage = document.getElementById("submittedMessage");
+
+  // Check if the message input is not empty
+  if (messageInput.value.trim() !== "") {
+    // Change the button text and disable it
+    submitButton.innerText = "Message Submitted";
+    submitButton.disabled = true;
+
+    // Display the submitted message
+    submittedMessage.innerText = "Thank you for your message!";
   }
+}
+
+// Show or hide the "Scroll to Top" button based on the scroll position
+window.onscroll = function () {
+  showScrollTopButton();
+};
+
+function showScrollTopButton() {
+  var scrollTopButton = document.getElementById("scrollTopBtn");
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollTopButton.classList.add("visible");
+  } else {
+    scrollTopButton.classList.remove("visible");
+  }
+}
+
+// Scroll to the top of the page smoothly
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
