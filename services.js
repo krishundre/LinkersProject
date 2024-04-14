12e140cac6598dcd735b276845b97b5272f07916
krishundre <krish.undre@somaiya.edu>
2024-04-14 17:38:41 +0530

    all services final completed

--- services.js
+++ services.js
@@ -250,7 +250,9 @@ function validateInput() {
     // Log a message or perform any other actions after copying
     console.log("Link copied to clipboard!");
   }
- countFB=1
+
+
+  countFB=1
   function generateQRCodeFB() {
     // Get the input field value
     var generatedLinkField=document.querySelector(".inputFB");
@@ -312,32 +314,40 @@ function validateInput() {
 
 
 
+
+
+
+
+
+
+
+
 // Email OPTIONS START FROM HERE
 function validateInputEM() {
-  // Get the input element
-  var inputElement=document.querySelector(".inputEM");
-
-  // Get the generate button
-  var generateBtnEM=document.getElementById("generateBtnEM");
-
-  var inputWarning=document.getElementById("input_warningEM");
-
-  // Show or hide the warning message based on input presence
-  inputWarning.style.display=inputElement.value.trim()===""? "block": "none";
-
-  // Enable the generate button if the input has data, otherwise disable it
-  generateBtnEM.disabled=inputElement.value.trim()==="";
-
+  var inputElement = document.querySelector(".inputEM");
+  var generateBtn = document.getElementById("generateBtnEM");
+  var inputWarning = document.getElementById("input_warningEM");
+  
+  inputWarning.style.display = inputElement.value.trim() === "" ? "block" : "none";
+  generateBtn.disabled = inputElement.value.trim() === "";
   console.log("Checking Input");
 }
 
 function generateLinkEM() {
 
   var inputElement=document.querySelector(".inputEM");
+  var inputElement_subj=document.querySelector(".inputEM_subj");
+  var inputElement_cc=document.querySelector(".inputEM_cc");
+  var inputElement_bcc=document.querySelector(".inputEM_bcc");
+  var inputElement_body=document.querySelector(".inputEM_body");
   var userInput=inputElement.value.trim(); // Get user input
+  var userInput_subj=inputElement_subj.value.trim(); // Get user input
+  var userInput_cc=inputElement_cc.value.trim(); // Get user input
+  var userInput_bcc=inputElement_bcc.value.trim(); // Get user input
+  var userInput_body=inputElement_body.value.trim(); // _bodyGet user input
 
   // Construct the full Instagram profile link with proper encoding
-  var generatedLink="https://mailto:"+encodeURIComponent(userInput);
+  var generatedLink="mailto:"+userInput+"?subject="+userInput_subj+"&cc="+userInput_cc+"&bcc="+userInput_bcc+"&body="+userInput_body;
   console.log(generatedLink); 
   // Set the generated link as the value of the input field
   inputElement.value=generatedLink;
@@ -345,9 +355,9 @@ function generateLinkEM() {
   // Open the generated link in a new tab if the action is 'test'
 
   // Enable buttons in the button_tray
-  var buttonTray=document.getElementById("buttons_holder");
-  buttonTray.classList.remove("disabled-buttons");
-
+  var buttonTray=document.getElementById("buttons_holderEM");
+  buttonTray.classList.remove("disabled-buttonsEM");
+  var scrollPosition = window.scrollY;
   // Restore the scroll position after the link is generated
   window.scrollTo(0, scrollPosition);
 
@@ -361,19 +371,108 @@ function generateLinkEM() {
   console.log("Buttons are open now");
 }
 
+function testLinkEM() {
+  // Construct the Instagram profile link
+  var inputElement=document.querySelector(".inputEM");
+  var userInput=inputElement.value.trim(); // Get user input
+
+  // Construct the full Instagram profile link with proper encoding
+  var testLink=userInput;
+
+  // Open the test link in a new tab
+  window.open(testLink, "_blank");
+
+  // Additional logic for testing the link goes here
+  // For demonstration purposes, we'll just log a message
+  console.log("Testing link...");
+}
+
+ function copyLinkEM() {
+    // Get the generated link input field
+    var generatedLinkField=document.querySelector(".inputEM");
+  
+    // Get the value of the input field
+    var inputValue=generatedLinkField.value;
+  
+    // Set the generated link as the value of the input field
+    generatedLinkField.value=inputValue;
+    
+      // Log a message or perform any other actions after copying
+      console.log("Link copied to clipboard!");
+  
+    // Select the text in the generated link input field
+    generatedLinkField.select();
+    // generatedLinkField.setSelectionRange(0, 99999);
+    /* For mobile devices */
+  
+    // Copy the selected text to the clipboard
+    document.execCommand("copy");
+  
+    // Deselect the text
+    generatedLinkField.blur();
+  }
+
+countEM=1;
+function generateQRCodeEM() {
+  console.log("generate !R")
+  // Get the input field value
+  var generatedLinkField=document.querySelector(".inputEM");
+
+  // Check if the input value is not empty
+  if (generatedLinkField !==""&& countEM==1) {
+    console.log(generatedLinkField.value)
+    // Create a QR code with the input value
+    var qrCode=new QRCode(document.getElementById("qr-codeEM"), {
+        text: generatedLinkField.value,
+        width: 200,
+        height: 200,
+      }
+
+    );
+    countEM++;
+    console.log("qr generated");
+
+    // Show the QR code section
+    document.getElementById("qr-code-sectionEM").style.display="block";
+  }
+
+  else {
+    alert("QR code already generated.");
+  }
+
+}
+
+function anchorcodeEM() {
+  // Get the generated link input field
+  var generatedLinkField=document.querySelector(".inputEM");
+
+  // Get the value of the input field
+  var inputValue=generatedLinkField.value;
+
+  // Construct the anchor tag code
+  var codeforweb="<a href=\""+inputValue+"\" title=\"EMail\">Click here to Mail us.</a>  // This code was generated on Linkers' Project. Visit us for more";
+
+  // Create a temporary textarea to copy the code
+  var tempTextarea=document.createElement("textarea");
+  tempTextarea.value=codeforweb;
+  document.body.appendChild(tempTextarea);
+
+  // Select the text in the textarea
+  tempTextarea.select();
+  tempTextarea.setSelectionRange(0, 99999);
+  /* For mobile devices */
+
+  // Copy the selected text to the clipboard
+  document.execCommand("copy");
+
+  // Remove the temporary textarea
+  document.body.removeChild(tempTextarea);
+
+  // Log a message or perform any other actions after copying
+  console.log("Link code copied to clipboard!");
+} 
+
 // Email OPTIONS END FROM HERE
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -482,7 +581,7 @@ function generateQRCodeWA() {
 
 }
 
-function anchorcode() {
+function anchorcodeWA() {
   // Get the generated link input field
   var generatedLinkField=document.querySelector(".inputWA");
 
@@ -512,57 +611,7 @@ function anchorcode() {
   console.log("Link code copied to clipboard!");
 } 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Whatsapp OPTIONS ENDS FROM HERE
 
 
 // Linkedin OPTIONS START FROM HERE
@@ -593,7 +642,6 @@ function anchorcode() {
   
     // Construct the Instagram profile link
     var inputElement=document.querySelector(".inputLK");
-    console.log(inputElement);
     var userInput=inputElement.value.trim(); // Get user input
   
     // Construct the full Instagram profile link with proper encoding
@@ -662,9 +710,66 @@ function anchorcode() {
     // Log a message or perform any other actions after copying
     console.log("Link copied to clipboard!");
   }
+  countLK=1;
+
+  function generateQRCodeLK() {
+    // Get the input field value
+    var generatedLinkFieldLK=document.querySelector(".inputLK");
   
-  var count=1;
+    // Check if the input value is not empty
+    if (generatedLinkFieldLK !==""&& countLK==1) {
+      console.log(generatedLinkFieldLK.value)
+      // Create a QR code with the input value
+      var qrCode=new QRCode(document.getElementById("qr-codeLK"), {
+          text: generatedLinkFieldLK.value,
+          width: 200,
+          height: 200,
+        }
   
+      );
+      countLK++;
+      console.log("qr generated");
   
+      // Show the QR code section
+      document.getElementById("qr-code-sectionLK").style.display="block";
+    }
   
+    else {
+      alert("QR code already generated.");
+    }
+  
+  }
+
+  function anchorcodeLK() {
+    // Get the generated link input field
+    var generatedLinkField=document.querySelector(".inputLK");
+  
+    // Get the value of the input field
+    var inputValue=generatedLinkField.value;
+  
+    // Construct the anchor tag code
+    var codeforweb="<a href=\""+inputValue+"\" title=\"Linkedin\">Click to visit us on Linkedin </a>  // This code was generated on Linkers' Project. Visit us for more";
+  
+    // Create a temporary textarea to copy the code
+    var tempTextarea=document.createElement("textarea");
+    tempTextarea.value=codeforweb;
+    document.body.appendChild(tempTextarea);
+  
+    // Select the text in the textarea
+    tempTextarea.select();
+    tempTextarea.setSelectionRange(0, 99999);
+    /* For mobile devices */
+  
+    // Copy the selected text to the clipboard
+    document.execCommand("copy");
+  
+    // Remove the temporary textarea
+    document.body.removeChild(tempTextarea);
+  
+    // Log a message or perform any other actions after copying
+    console.log("Link code copied to clipboard!");
+  } 
+  
+  // Linkedin OPTIONS END FROM HERE
+
   
